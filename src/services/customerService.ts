fdc697dd1db47767e087f5f99b1f549f8d6b1019
millany.cua <millany.cua@paymaya.com>
2023-02-23 10:46:11 +0800

    added update and delete customer in customer service

--- src/services/customerService.ts
+++ src/services/customerService.ts
@@ -15,3 +15,25 @@ export const createCustomer = async (req: CreateCustomerRequest) => {
     const customer: Customer = response.data
     return customer
 }
+
+export const updateCustomer = async (customerId: string, req: CreateCustomerRequest) => {
+    const headers = {
+        accept: 'application/json',
+        authorization: `Basic ${secretAuth}`,
+    }
+    
+    const response = await axios.put(`${mayaPaymentsUrl}/customers/${customerId}`, req, { headers })
+    const customer: Customer = response.data
+    return customer
+}
+
+export const deleteCustomer = async (customerId: string) => {
+    const headers = {
+        accept: 'application/json',
+        authorization: `Basic ${secretAuth}`,
+    }
+    
+    const response = await axios.delete(`${mayaPaymentsUrl}/customers/${customerId}`, { headers })
+    const customer: Customer = response.data
+    return customer
+}
