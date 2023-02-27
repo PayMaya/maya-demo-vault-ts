e9bbc6cbd47d8b18f19a4dbd1ec528bf1013e6a5
millany.cua <millany.cua@paymaya.com>
2023-02-27 15:05:27 +0800

    minor cleanup

--- src/components/VaultedCard.tsx
+++ src/components/VaultedCard.tsx
@@ -1,7 +1,6 @@
 import { Dispatch } from 'redux';
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { makeCardDefaultThunk } from '../thunks/card';
-import { useSelector } from 'react-redux';
 
 function VaultedCard ({ cardTokenId, default: isDefault, cardType, first6, last4 }: CardDetails) {
     const mayaCustomerId: string = useSelector((state: AppState) => state.currentUser.mayaCustomerId!)
