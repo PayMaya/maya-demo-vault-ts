13de93ecc0b69802d695d7589a574192ba2866f1
Karl Edward Bayron <karl.bayron@paymaya.com>
2023-03-02 16:06:09 +0800

    Add caret symbol for accordion

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -8,10 +8,13 @@ interface AccordionProps {
 export function Accordion({ header, children, group, index }: AccordionProps) {
     return (
         <div className="accordion-container">
-            <label htmlFor={`accordion-state-${index}`}>
-                <div className="accordion-button">{ header }</div>
-            </label>
             <input type="radio" className="accordion-state" id={`accordion-state-${index}`} name={ group } hidden/>
+            <label htmlFor={`accordion-state-${index}`}>
+                <div className="accordion-button">
+                    { header }
+                    <p className="accordion-caret">&#9660;</p>
+                </div>
+            </label>
             <div className="accordion-content">
                 <div className="accordion-inner">
                    {children} 
